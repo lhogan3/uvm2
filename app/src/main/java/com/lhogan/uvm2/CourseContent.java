76d8ae382d8e52ea2eb7666752d16d5fbc83302c
billiamvt <billiamh@live.com>
2019-11-26 19:24:01 -0500

    course list -> my course list

--- app/src/main/java/com/lhogan/uvm2/CourseContent.java
+++ app/src/main/java/com/lhogan/uvm2/CourseContent.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import java.util.Scanner;
 
 import static com.lbelivea.uvm2.CourseListActivity.SimpleItemRecyclerViewAdapter.resetAdapter;
+import static com.lbelivea.uvm2.LoggedInUser.findUserCourses;
 
 
 public class CourseContent {
@@ -46,6 +47,8 @@ public class CourseContent {
                     }
 
                 }
+
+                findUserCourses();
             } catch (IOException e) {
                 Log.d("ERROR", "doInBackground: IO ERROR");
             }
